6d36527fa929937ebe2b1015e2ad8f79259f06d1
Miroslav Gavrilov <gavrilov.miroslav@gmail.com>
2018-04-07 03:42:03 -0700

    Moved binaries and dumps

---
+++ bin/drivers/DriverIndexOf.java
@@ -0,0 +1,15 @@
+
+
+public class DriverIndexOf {
+	public static void main(String[] args) {
+		char[] source;
+		int sourceOffset;
+		int sourceCount;
+		char[] target;
+		int targetOffset;
+		int targetCount;
+		int fromIndex;
+		String test;
+		test.indexOf(source, sourceOffset, sourceCount, target, targetOffset, targetCount, fromIndex);
+	}
+}
--- bin/drivers/DriverToString.java
+++ bin/drivers/DriverToString.java
@@ -2,11 +2,8 @@
 
 public class DriverToString {
 	public static void main(String[] args) {
-		BigInteger u;
-		StringBuilder sb;
 		int radix;
-		int digits;
 		BigInteger test;
-		test.toString(u, sb, radix, digits);
+		test.toString(radix);
 	}
 }
